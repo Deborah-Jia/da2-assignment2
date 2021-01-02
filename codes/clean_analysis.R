ebb2b8d00eaa7df88a409867bba4f566bf1e6613
Deborah-Jia <vermouth2016@gmail.com>
2021-01-02 13:17:27 +0100

    add folders

--- codes/clean_analysis.R
+++ codes/clean_analysis.R
@@ -3,8 +3,8 @@ library(tidyverse)
 library(dplyr)
 library(RCurl)
 
-url_git <- url("https://github.com/Deborah-Jia/da2-assignment2/blob/main/data/raw/b_f_raw1.csv", "rb")
-df2 <- read_html(url_git)
+my_url <- url("https://raw.githubusercontent.com/Deborah-Jia/da2-assignment2/main/data/raw/b_f_raw1.csv")
+df2 <- read_delim(my_url, delim = ;)
 
 df2 <- df2[-c(38),]
 
@@ -17,3 +17,4 @@ df2 <- df2 %>% rename(life_sa = `Life satisfaction`, employ_r = `Employment rate
 my_path <- "/Users/wodediannao/Desktop/da2-assignment2/data/"
 
 write_csv( df2 , paste0(my_path,'clean/b_f_clean.csv'))
+
